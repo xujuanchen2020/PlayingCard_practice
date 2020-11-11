a918a46692de281625ca5c324f1fb87abdd9d15b
Xujuan Chen <xujuan.chen@lc.cuny.edu>
2020-11-11 17:42:37 -0500

    Initial Commit

---
+++ PlayingCard_practice/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  PlayingCard_practice
+//
+//  Created by qiurong chen on 11/9/20.
+//  Copyright © 2020 Xujuan Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var deck = PlayingCardDeck()
+
+    @IBOutlet weak var playingCardView: PlayingCardView! {
+        didSet {
+            let swipe = UISwipeGestureRecognizer(target: self, action: #selector(nextCard))
+            swipe.direction = [.left, .right]
+            playingCardView.addGestureRecognizer(swipe)
+            
+            let pich = UIPinchGestureRecognizer(target: playingCardView, action: #selector(playingCardView.adjustFaceCardScale(byHandlingGestureRecognizedBy:)))
+            playingCardView.addGestureRecognizer(pich)
+        }
+    }
+    
+    @IBAction func flipCard(_ sender: UITapGestureRecognizer) {
+        switch sender.state {
+        case .ended:
+            playingCardView.isFaceUp = !playingCardView.isFaceUp
+        default: break
+        }
+    }
+    
+    @objc func nextCard() {
+        if let card = deck.draw() {
+            playingCardView.rank = card.rank.order
+            playingCardView.suit = card.suit.rawValue
+        }
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+}
+
